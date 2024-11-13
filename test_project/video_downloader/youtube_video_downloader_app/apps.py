e2bec9800093e1d07523211bdbdb690bfb6d515a
Vishnu B <vishnuu3043@gmail.com>
2024-11-13 17:26:31 +0530

    created project

---
+++ test_project/video_downloader/youtube_video_downloader_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YoutubeVideoDownloaderAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'youtube_video_downloader_app'
---
+++ test_project/video_downloader/youtube_video_downloader_app/views.py
@@ -0,0 +1,21 @@
+# importing all the required modules
+from django.shortcuts import render, redirect
+from pytube import *
+
+
+def youtube(request):
+    # checking whether request.method is post or not
+    if request.method == 'POST':
+        # getting link from frontend
+        link = request.POST['link']
+        video = YouTube(link)
+
+        # setting video resolution
+        stream = video.streams.get_lowest_resolution()
+
+        # downloads video
+        stream.download()
+
+        # returning HTML page
+        return render(request, 'index.html')
+    return render(request, 'index.html')
